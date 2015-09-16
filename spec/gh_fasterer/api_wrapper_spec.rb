bc3898b50919f52c36555d4d49bdf8a9a19e471a
Kacper Goliński <kacper.golinski@netguru.co>
2015-09-16 19:12:35 +0200

    use vcr in api_wrapper spec

--- spec/gh_fasterer/api_wrapper_spec.rb
+++ spec/gh_fasterer/api_wrapper_spec.rb
@@ -5,32 +5,44 @@ describe GhFasterer::ApiWrapper do
   describe '#contents' do
     subject { described_class.new(owner, repo).contents(path) }
 
-    let(:owner) { 'owner' }
-    let(:repo) { 'repo' }
+    let(:owner) { 'caspg' }
+    let(:repo) { 'gh_fasterer' }
 
-    before do
+    before(stub_request: true) do
       allow(HTTParty).to receive(:get) { StubbedResponse.new }
     end
 
     context 'when path is nil' do
       let(:path) { nil }
 
-      it 'makes request with correct url' do
+      it 'makes request with correct url', stub_request: true do
         subject
 
-        url = 'https://api.github.com/repos/owner/repo/contents/'
+        url = 'https://api.github.com/repos/caspg/gh_fasterer/contents/'
         expect(HTTParty).to have_received(:get).with(url)
+      end
+
+      it 'returns an array with files and folders data' do
+        VCR.use_cassette('folder_data') do
+          expect(subject.parsed_response.class).to eq(Array)
+        end
       end
     end
 
     context 'when path is not nil' do
-      let(:path) { 'path/to/file.rb' }
+      let(:path) { 'lib/gh_fasterer.rb' }
 
-      it 'makes request with correct url' do
+      it 'makes request with correct url', stub_request: true do
         subject
 
-        url = 'https://api.github.com/repos/owner/repo/contents/path/to/file.rb'
+        url = 'https://api.github.com/repos/caspg/gh_fasterer/contents/lib/gh_fasterer.rb'
         expect(HTTParty).to have_received(:get).with(url)
+      end
+
+      it 'returns a hash with file data' do
+        VCR.use_cassette('file_data') do
+          expect(subject.parsed_response.class).to eq(Hash)
+        end
       end
     end
   end
