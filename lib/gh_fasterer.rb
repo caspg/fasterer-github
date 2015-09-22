d1d0c978aca031c9b0052b2f219276635acc3d98
Kacper Goliński <kacper.golinski@netguru.co>
2015-09-22 20:01:27 +0200

    add main function to gh_fasterer

--- lib/gh_fasterer.rb
+++ lib/gh_fasterer.rb
@@ -1,5 +1,11 @@
 require 'gh_fasterer/version'
 
 module GhFasterer
-  # Your code goes here...
+  class << self
+    def scan(owner:, repo:, path: nil)
+      scanner = GhFasterer::Scanner.new(owner, repo, path)
+      scanner.run
+      scanner.results
+    end
+  end
 end
