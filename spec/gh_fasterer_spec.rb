95fe4ccbccf73e4641c1cfc5c4f0cfa53ea9f396
Kacper Goliński <kacper.golinski@netguru.co>
2015-10-07 19:53:27 +0200

    update gh_fasterer_spec

--- spec/gh_fasterer_spec.rb
+++ spec/gh_fasterer_spec.rb
@@ -4,4 +4,18 @@ describe GhFasterer do
   it 'has a version number' do
     expect(GhFasterer::VERSION).not_to be nil
   end
+
+  describe '.scan' do
+    let(:owner) { 'owner' }
+    let(:repo) { 'repo' }
+
+    it 'calls correct methods' do
+      scanner = double
+      expect(GhFasterer::Scanner).to receive(:new).with(owner, repo, nil) { scanner }
+      expect(scanner).to receive(:run)
+      expect(scanner).to receive(:results)
+
+      subject.scan(owner: owner, repo: repo)
+    end
+  end
 end
