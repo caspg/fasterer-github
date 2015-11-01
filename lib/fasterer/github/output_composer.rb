8494712c5762c17033882232761bc78188bf4783
Kacper Golinski <kacper.golinski@netguru.co>
2015-11-01 19:05:30 +0100

    allow adding ignored offences in '.fasterer.yaml' file

--- lib/fasterer/github/output_composer.rb
+++ lib/fasterer/github/output_composer.rb
@@ -1,6 +1,10 @@
 module Fasterer
   module Github
     class OutputComposer
+
+      CONFIG_FILE_NAME = '.fasterer.yml'
+      SPEEDUPS_KEY     = 'speedups'
+
       def initialize(owner, repo)
         @repo_owner = owner
         @repo_name = repo
@@ -9,6 +13,7 @@ module Fasterer
       def add_offences(offences, path)
         offences.each do |offence_name, lines|
           details = { path: path, lines: lines }
+          next if ignored_offences.include?(offence_name)
           (fasterer_offences[offence_name] ||= []) << details
         end
       end
@@ -45,6 +50,14 @@ module Fasterer
 
       def api_errors
         @api_errors ||= []
+      end
+
+      def ignored_offences
+        @ignored_offences ||= load_ignored_offences[SPEEDUPS_KEY].select { |_, v| v == false }.keys.map(&:to_sym)
+      end
+
+      def load_ignored_offences
+        YAML.load_file(File.join(Dir.pwd, CONFIG_FILE_NAME))
       end
     end
   end
